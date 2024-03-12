9eb272688b7931e2003ccfa72b73251811615902
Sohaib Kaidali <kaidalisohaib@gmail.com>
2024-03-12 03:17:45 -0400

    Improved website + demo =)

--- website-related/js-css/prediction.js
+++ website-related/js-css/prediction.js
@@ -1,38 +1,115 @@
-// import ONNXRuntime Web from CDN
+'use strict';
+
+// Import ONNXRuntime Web from CDN
 import * as ort from "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.17.1/dist/esm/ort.min.js";
 
-// set wasm path override
+// Set wasm path override and number of threads
 ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.17.1/dist/";
 ort.env.wasm.numThreads = 1;
 
-const modelUrl = "https://raw.githubusercontent.com/kaidalisohaib/AICohortW24/main/models/best_model.onnx"
-// use an async context to call onnxruntime functions.
+// URL of the ONNX model to be used for prediction
+const modelUrl = "https://raw.githubusercontent.com/kaidalisohaib/AICohortW24/main/models/best_model.onnx";
+
+// DOM elements
+const predictionOutput = document.getElementById("predictionOutput");
+const makePredictButton = document.getElementById("makePredictButton");
+const form = document.getElementById('predictionForm');
+
+// Data object to hold form field values
+const data = {};
+let isPredicting = false;
+
+// Add Bootstrap validation to all forms with '.needs-validation' class
+let forms = document.querySelectorAll('.needs-validation');
+Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+
+// Event listener for form submission
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // Check if a prediction is already in progress
+    if (isPredicting) return;
+    isPredicting = true;
+
+    // Check form validity
+    if (!form.checkValidity()) {
+        isPredicting = false;
+        return;
+    }
+
+    // Update UI to indicate prediction in progress
+    makePredictButton.classList.toggle("disabled");
+    makePredictButton.children[0].classList.toggle("visually-hidden");
+    makePredictButton.children[1].innerText = "Making prediction...";
+
+    // Loop through all form fields and populate the data object
+    for (const field of form.elements) {
+        if (field.name && field.value) {
+            data[field.name] = field.value;
+        }
+    }
+
+    // Prepare the input array in the correct order
+    const orderedArray = [
+        data.HighBP, data.HighChol, data.CholCheck, data.BMI, data.Smoker,
+        data.Stroke, data.Diabetes, data.PhysActivity, data.Fruits, data.Veggies,
+        data.HvyAlcoholConsump, data.AnyHealthcare, data.NoDocbcCost, data.GenHlth,
+        data.MentHlth, data.PhysHlth, data.DiffWalk, data.Sex, data.Age, data.Education,
+        data.Income
+    ];
+
+    // console.log('Form Data:', data);
+    // console.log('Form array:', orderedArray);
+
+    // Make the prediction and display the result
+    let outputPercentage = await main(orderedArray);
+    outputPercentage = (outputPercentage * 100).toFixed(1);
+
+    makePredictButton.classList.toggle("disabled");
+    makePredictButton.children[0].classList.toggle("visually-hidden");
+    makePredictButton.children[1].innerText = "Make prediction";
+    predictionOutput.innerText = `Likelihood of Heart Disease: ${outputPercentage}%`;
+    isPredicting = false;
+});
+
+// Update prediction output on form input
+form.addEventListener("input", () => {
+    predictionOutput.innerText = "Waiting for input ...";
+});
+
+// Function to make the prediction using the ONNX model
 async function main(featuresArray) {
     try {
-        // create a new session and load the specific model.
-        //
-        // the model in this example contains a single MatMul node
+        // Create a new session and load the ONNX model
         const session = await ort.InferenceSession.create(modelUrl);
 
-        // prepare inputs. a tensor need its corresponding TypedArray as data
+        // Prepare the input tensor from the feature array
         const dataInput = Float32Array.from(featuresArray);
         const tensorInput = new ort.Tensor('float32', dataInput, [1, 21]);
 
-        // prepare feeds. use model input names as keys.
+        // Prepare the input feeds for the model
         const feeds = { "input": tensorInput };
 
-        // feed inputs and run
+        // Run inference and get the output
         const results = await session.run(feeds);
 
-        console.log('output_label', results.label.data)
-        console.log('output_probability', results.output.data)
-        // document.write("Input: ", dataInput)
-        // document.write(`Prediction: ${results.label.data[0]}`)
-
+        // console.log('output_label', Number(results.label.data[0]));
+        // console.log('output_probability', results.output.data[1]);
+        return results.output.data[1];
     } catch (e) {
-        console.error(`failed to inference ONNX model: ${e}.`);
+        console.error(`Failed to perform inference: ${e}`);
     }
 }
 
-let input = [1., 1., 1., 40., 1., 0., 0., 0., 0., 1., 0., 1., 0., 5., 18., 15., 1., 0., 9., 4., 3.]
-main(input);
+// Example usage of the main function
+// let input = [1., 1., 1., 40., 1., 0., 0., 0., 0., 1., 0., 1., 0., 5., 18., 15., 1., 0., 9., 4., 3.];
+// main(input);
